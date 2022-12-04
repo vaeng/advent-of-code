28502283a1797535d169b4f055185d04ffbf73af
Christian Willner <christiaaan@gmail.com>
2022-12-04 08:04:06 +0100

    Solve day 4 2022

--- 2022/Elixir/lib/advent_of_code/day_04.ex
+++ 2022/Elixir/lib/advent_of_code/day_04.ex
@@ -1,7 +1,16 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def part1(args) do
+    args|> String.split() |> Enum.map(&String.split(&1, ~r{[\-\,]})) |> Enum.map(&Enum.map(&1, fn x -> String.to_integer(x) end)) |> Enum.map(fn [a,b,c,d] -> (a >= c and b <= d) or (c >= a and d <= b)  end) |> Enum.count(fn x -> x end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.split()
+    |> Enum.map(&String.split(&1, ~r{[\-\,]}))
+    |> Enum.map(&Enum.map(&1, fn x -> String.to_integer(x) end))
+    |> Enum.map(fn [a,b,c,d] -> (a >= c and a <= d) or (b >= c and b <= d) or (c >= a and c <= b) or (d >= a and d <= b)
+   end)
+    |> Enum.count(fn x -> x end)
+
   end
 end
