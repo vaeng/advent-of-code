229b8483854dbabcfcc2d60cda11ac591bfc2619
vaeng <christiaaan@gmail.com>
2022-12-09 10:20:12 +0000

    Start solve day 9 2022

--- 2022/Elixir/lib/advent_of_code/day_08.ex
+++ 2022/Elixir/lib/advent_of_code/day_08.ex
@@ -9,8 +9,10 @@ defmodule AdventOfCode.Day08 do
     input
     |> String.trim()
     |> String.split("\n")
-    |> Enum.map(&(String.codepoints(&1)
-                   |> Enum.map(fn x -> String.to_integer(x) end)))
+    |> Enum.map(
+      &(String.codepoints(&1)
+        |> Enum.map(fn x -> String.to_integer(x) end))
+    )
   end
 
   def setAllToUnknown(numgrid) do
@@ -19,22 +21,63 @@ defmodule AdventOfCode.Day08 do
 
   def initializeBoarders(tagGrid) do
     tagGrid
-    |> setFirstRowVis() # top
-    |> transpose()
-    |> setFirstRowVis() # left
-    |> transpose()
-    |> setFirstRowVis() # down
-    |> transpose()
-    |> setFirstRowVis() # right
-    |> transpose() # return in original orientation
+    |> List.update_at(-1, &Enum.map(&1, fn [x, _] -> [x, :visible] end))
+    |> List.update_at(0, &Enum.map(&1, fn [x, _] -> [x, :visible] end))
+    |> Enum.map(fn [[x, _] | rest] -> [[x, :visible] | rest] end)
+    |> Enum.map(&List.update_at(&1, -1, fn [x, _] -> [x, :visible] end))
   end
 
-  def setFirstRowVis([firstRow | rest]) do
-    visFirst = firstRow |> Enum.map( fn [x, _] -> [x, :vis] end)
-    [visFirst | rest]
+  def updateStatus(_, _, _, :visible, _), do: :visible
+  def updateStatus(_, _, _, :invisible, _), do: :invisible
+
+  def updateStatus(rix, cix, height, :unknown, grid) do
+    # [[hup, sup], [hdown, sdown], [hleft, sleft], [hright, sright]] = getEnvironment(grid, rix, cix)
+    getEnvironment(grid, rix, cix) |> Enum.any?()
+
+    # cases for: all blocking -> invis
+    # any is smaller and vis? -> vis
+    # else -> unknown
   end
 
-  def transpose(rows) do
-    rows |> List.zip|> Enum.map(&Tuple.to_list/1)
+  def updateStatus(_, _, _, _, _), do: :error
+
+  def updateGrid(tagGrid) do
+    newStatus = fn _, _, _, _ -> :test end
+
+    workList =
+      tagGrid
+      |> Enum.with_index()
+      |> Enum.map(fn {row, rix} ->
+        Enum.with_index(row, fn element, cix -> [rix, cix, element] end)
+      end)
+      |> List.flatten()
+      |> Enum.chunk_every(4)
+
+    workList
+    |> Enum.reduce(tagGrid, fn [rix, cix, height, status], accgrid ->
+      List.update_at(
+        accgrid,
+        rix,
+        &List.update_at(&1, cix, fn [x, status] ->
+          [x, updateStatus(rix, cix, height, status, accgrid)]
+        end)
+      )
+    end)
+  end
+
+  def getVisibleTrees(grid), do: grid |> List.flatten() |> Enum.count(&(&1 == :visible))
+
+  def updateStatusByCoordinate(grid, rix, cix, newStatus),
+    do: grid |> List.update_at(rix, &List.update_at(&1, cix, fn [x, _] -> [x, newStatus] end))
+
+  def getElement(grid, rix, cix),
+    do: grid |> Enum.fetch(rix) |> then(fn {_, row} -> Enum.fetch!(row, cix) end)
+
+  def(getEnvironment(grid, rix, cix)) do
+    up = getElement(grid, rix - 1, cix)
+    down = getElement(grid, rix + 1, cix)
+    left = getElement(grid, rix, cix - 1)
+    right = getElement(grid, rix, cix + 1)
+    [up, down, left, right]
   end
 end
--- 2022/Elixir/lib/advent_of_code/day_09.ex
+++ 2022/Elixir/lib/advent_of_code/day_09.ex
@@ -1,7 +1,56 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(args) do
+    instructions = parseInput(args)
+
+    headPositions =
+      instructions
+      |> String.codepoints()
+      |> Enum.scan([0, 0], fn x, acc -> AdventOfCode.Day09.updateHead(acc, x) end)
+
+    headPositions
+    |> Enum.scan([0, 0], fn head, acc -> AdventOfCode.Day09.updateTail(head, acc) end)
+    |> MapSet.new()
+    |> MapSet.size()
   end
 
   def part2(_args) do
+  end
+
+  def parseInput(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(
+      &(String.split(&1, " ")
+        |> then(fn [dir, num] -> String.duplicate(dir, String.to_integer(num)) end))
+    )
+    |> Enum.reduce("", fn x, y -> y <> x end)
+  end
+
+  def updateHead([x, y], "R"), do: [x + 1, y]
+  def updateHead([x, y], "L"), do: [x - 1, y]
+  def updateHead([x, y], "U"), do: [x, y + 1]
+  def updateHead([x, y], "D"), do: [x, y - 1]
+
+  def updateTail([xh, yh], [xt, yt]) do
+    newXt =
+      cond do
+        yh == yt and xh - xt > 1 -> xt + 1
+        yh == yt and xh - xt < -1 -> xt - 1
+        yh > yt + 1 and xh > xt -> xt + 1
+        yh < yt -1  and xh < xt -> xt - 1
+        true -> xt
+      end
+
+    newYt =
+      cond do
+        xh == xt and yh - yt > 1 -> yt + 1
+        xh == xt and yh - yt < -1 -> yt - 1
+        xh > xt + 1 and yh > yt -> yt + 1
+        xh < xt - 1 and yh < yt -> yt - 1
+        true -> yt
+      end
+
+    [newXt, newYt]
   end
 end
