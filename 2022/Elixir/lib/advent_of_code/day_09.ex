eff887e2e3b12a28e61b878389198fd8a8244a78
vaeng <christiaaan@gmail.com>
2022-12-09 21:18:34 +0000

    Solve day 9 2022

--- 2022/Elixir/lib/advent_of_code/day_09.ex
+++ 2022/Elixir/lib/advent_of_code/day_09.ex
@@ -1,10 +1,21 @@
 defmodule AdventOfCode.Day09 do
+  @moduledoc """
+  This module is using named directions in a 2D Space.
+  The center X be [0,0]. eg. NN is [0,2].
+  |      |     |    |     |      |
+  |:----:|:---:|:---:|:---:|:----:|
+  | NWNW | NNW | NN | NNE | NENE |
+  | NWW  | NW  | N  | NE  | NEE  |
+  | WW   | W   | X  | E   | EE   |
+  | SWW  | SW  | S  | SE  | SEE  |
+  | SWSW | SSW | SS | SSE | SESE |
+  """
   def part1(args) do
     args
     |> parseInput()
     |> headPositions()
     # 1
-    |> tailPositions()
+    |> nextKnotPositions()
     |> MapSet.new()
     |> MapSet.size()
   end
@@ -13,10 +24,8 @@ defmodule AdventOfCode.Day09 do
     args
     |> parseInput()
     |> headPositions()
-    |> IO.inspect()
     # 1
     |> nextKnotPositions()
-
     # 2
     |> nextKnotPositions()
     # 3
@@ -37,23 +46,15 @@ defmodule AdventOfCode.Day09 do
     |> MapSet.size()
   end
 
-  def draw(positions) do
-  end
-
   def headPositions(instructions) do
     instructions
     |> String.codepoints()
     |> Enum.scan([0, 0], fn x, acc -> AdventOfCode.Day09.updateHead(acc, x) end)
   end
 
-  def tailPositions(previousKnotPositions) do
-    previousKnotPositions
-    |> Enum.scan([0, 0], fn head, acc -> AdventOfCode.Day09.updateTail(head, acc) end)
-  end
-
   def nextKnotPositions(previousKnotPositions) do
     previousKnotPositions
-    |> Enum.scan([0, 0], fn head, acc -> AdventOfCode.Day09.updateTailPart2(head, acc) end)
+    |> Enum.scan([0, 0], fn head, acc -> AdventOfCode.Day09.updateKnot(head, acc) end)
   end
 
   def parseInput(input) do
@@ -64,37 +65,17 @@ defmodule AdventOfCode.Day09 do
       &(String.split(&1, " ")
         |> then(fn [dir, num] -> String.duplicate(dir, String.to_integer(num)) end))
     )
-    |> Enum.reduce("", fn x, y -> y <> x end)
+    |> Enum.reduce("", &(&2 <> &1))
   end
 
-  def updateHead([x, y], "R"), do: [x + 1, y]
-  def updateHead([x, y], "L"), do: [x - 1, y]
-  def updateHead([x, y], "U"), do: [x, y + 1]
-  def updateHead([x, y], "D"), do: [x, y - 1]
+  def updateHead(h, "R"), do: getE(h)
+  def updateHead(h, "L"), do: getW(h)
+  def updateHead(h, "U"), do: getN(h)
+  def updateHead(h, "D"), do: getS(h)
 
-  def updateTail([xh, yh], [xt, yt]) do
+  def updateKnot(h, t) do
     cond do
-      # same row, head right
-      yh == yt and xh - xt > 1 -> [xt + 1, yt]
-      # same row head left
-      yh == yt and xh - xt < -1 -> [xt - 1, yt]
-      # same line head up
-      xh == xt and yh - yt > 1 -> [xt, yt + 1]
-      # same line head down
-      xh == xt and yh - yt < -1 -> [xt, yt - 1]
-      # head is two up and right
-      yh > yt + 1 and xh != xt -> [xh, yt + 1]
-      # head is two down and left
-      yh < yt - 1 and xh != xt -> [xh, yt - 1]
-      xh > xt + 1 and yh != yt -> [xt + 1, yh]
-      xh < xt - 1 and yh != yt -> [xt - 1, yh]
-      abs(xh - xt) <= 1 and abs(yh - yt) <= 1 -> [xt, yt]
-      true -> IO.inspect("currently: head: [#{xh},#{yh}] tail: [#{xt},#{yt}]\n")
-    end
-  end
-
-  def updateTailPart2(h, t) do
-    cond do
+      h == t -> t
       h == getN(t) -> t
       h == getNN(t) -> getN(t)
       h == getS(t) -> t
@@ -103,57 +84,52 @@ defmodule AdventOfCode.Day09 do
       h == getWW(t) -> getW(t)
       h == getE(t) -> t
       h == getEE(t) -> getE(t)
-
       h == getSE(t) -> t
       h == getSSE(t) -> getSE(t)
       h == getSEE(t) -> getSE(t)
       h == getSESE(t) -> getSE(t)
-
       h == getNE(t) -> t
       h == getNNE(t) -> getNE(t)
       h == getNEE(t) -> getNE(t)
       h == getNENE(t) -> getNE(t)
-
       h == getSW(t) -> t
       h == getSSW(t) -> getSW(t)
       h == getSWW(t) -> getSW(t)
       h == getSWSW(t) -> getSW(t)
-
       h == getNW(t) -> t
       h == getNNW(t) -> getNW(t)
       h == getNWW(t) -> getNW(t)
       h == getNWNW(t) -> getNW(t)
-      true -> IO.inspect("currently: head: [#{h}] tail: [#{t}]\n")
+      true -> raise "unknown skip"
     end
   end
 
-  def getN([x, y]), do: [x, y+1]
-  def getNN([x, y]), do: [x, y+2]
-  def getS([x, y]), do: [x, y-1]
-  def getSS([x, y]), do: [x, y-2]
-  def getW([x, y]), do: [x-1, y]
-  def getWW([x, y]), do: [x-2,y]
-  def getE([x, y]), do: [x+1, y]
-  def getEE([x, y]), do: [x+2,y]
+  def getN([x, y]), do: [x, y + 1]
+  def getNN([x, y]), do: [x, y + 2]
+  def getS([x, y]), do: [x, y - 1]
+  def getSS([x, y]), do: [x, y - 2]
+  def getW([x, y]), do: [x - 1, y]
+  def getWW([x, y]), do: [x - 2, y]
+  def getE([x, y]), do: [x + 1, y]
+  def getEE([x, y]), do: [x + 2, y]
 
-  def getSE([x, y]), do: [x+1,y-1]
-  def getSSE([x, y]), do: [x+1,y-2]
-  def getSEE([x, y]), do: [x+2,y-1]
-  def getSESE([x, y]), do: [x+2,y-2]
+  def getSE([x, y]), do: [x + 1, y - 1]
+  def getSSE([x, y]), do: [x + 1, y - 2]
+  def getSEE([x, y]), do: [x + 2, y - 1]
+  def getSESE([x, y]), do: [x + 2, y - 2]
 
-  def getNE([x, y]), do: [x+1,y+1]
-  def getNNE([x, y]), do: [x+1,y+2]
-  def getNEE([x, y]), do: [x+2,y+1]
-  def getNENE([x, y]), do: [x+2,y+2]
+  def getNE([x, y]), do: [x + 1, y + 1]
+  def getNNE([x, y]), do: [x + 1, y + 2]
+  def getNEE([x, y]), do: [x + 2, y + 1]
+  def getNENE([x, y]), do: [x + 2, y + 2]
 
-  def getSW([x, y]), do: [x-1,y-1]
-  def getSSW([x, y]), do: [x-1,y-2]
-  def getSWW([x, y]), do: [x-2,y-1]
-  def getSWSW([x, y]), do: [x-2,y-2]
+  def getSW([x, y]), do: [x - 1, y - 1]
+  def getSSW([x, y]), do: [x - 1, y - 2]
+  def getSWW([x, y]), do: [x - 2, y - 1]
+  def getSWSW([x, y]), do: [x - 2, y - 2]
 
-  def getNW([x, y]), do: [x-1,y+1]
-  def getNNW([x, y]), do: [x-1,y+2]
-  def getNWW([x, y]), do: [x-2,y+1]
-  def getNWNW([x, y]), do: [x-2,y+2]
-
+  def getNW([x, y]), do: [x - 1, y + 1]
+  def getNNW([x, y]), do: [x - 1, y + 2]
+  def getNWW([x, y]), do: [x - 2, y + 1]
+  def getNWNW([x, y]), do: [x - 2, y + 2]
 end
