e2cc6abe26977c1c4f29b944a2c8d25d7c51c610
vaeng <34183939+vaeng@users.noreply.github.com>
2022-12-25 13:08:29 +0000

    Add solution for day 25 2022

--- 2022/Elixir/lib/advent_of_code/day_25.ex
+++ 2022/Elixir/lib/advent_of_code/day_25.ex
@@ -1,7 +1,67 @@
 defmodule AdventOfCode.Day25 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&convert_from_snafu/1)
+    |> Enum.sum()
+    |> (then &convert_to_snafu/1)
   end
 
   def part2(_args) do
   end
+
+  def convert_letter(c) do
+    case c do
+      "=" -> "0"
+      "-" -> "1"
+      "0" -> "2"
+      "1" -> "3"
+      "2" -> "4"
+    end
+  end
+
+  def convert_digit(c) do
+    case c do
+      2 -> "0"
+      3 -> "1"
+      4 -> "2"
+      1 -> "-"
+      0 -> "="
+    end
+  end
+
+  def convert_from_snafu(string) do
+    bond = string
+    |> String.trim()
+    |> String.length()
+    |> (then &List.duplicate("0", &1))
+    |> Enum.map(&convert_letter/1)
+    |> Enum.join()
+    |> String.to_integer(5)
+    num = string
+    |> String.trim()
+    |> String.codepoints()
+    |> Enum.map(&convert_letter/1)
+    |> Enum.join()
+    |> String.to_integer(5)
+
+    num - bond
+  end
+
+  def convert_to_snafu(number) do
+    bound = Integer.undigits(List.duplicate(2, Enum.count(Integer.digits(number, 5))+1), 5)
+    Integer.digits(bound - number, 5)
+    |> Enum.map(fn n -> case n do
+      1 -> "1"
+      2 -> "0"
+      3 -> "-"
+      4 -> "="
+      0 -> "2"
+    end
+    end)
+    |> Enum.join()
+    |> String.trim_leading("0")
+  end
+
 end
